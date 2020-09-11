e88612925c9f5b3a6ed1d059c74ad5a30bfc320a
kittipat-088 <s6035512088@phuket.psu.ac.th>
2020-09-11 09:34:45 +0700

    check 1

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      Hello World (2+3+4)
+    </div>
+  );
+}
+
+export default App;
