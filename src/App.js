b664bb2f94369fa3f6638d04bee05822889997ad
rewkong1150 <70642159+rewkong1150@users.noreply.github.com>
2020-09-15 13:08:23 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect,useRef } from 'react';
-import WordCard from './component/WordCard'
+import WordCard from './WordCard'
 import './App.css'
 import _ from 'lodash';
 function App() {
